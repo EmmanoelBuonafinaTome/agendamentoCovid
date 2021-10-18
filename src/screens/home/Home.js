88cc0c60043f5d4840c99ec7a3ccd39a8834d10a
emmanoel1965@gmail.com <emmanoel1965@gmail.com>
2021-10-18 16:55:15 -0300

    create sreen home

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,69 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Title } from 'react-native-paper';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return(
-    <View>
-      <Text>Alo</Text>
+    <View style={styles.mainView}>
+      <View style={styles.viewImage}>
+        <Image 
+          style={styles.mainImageHome}
+          source={require('../../assets/images/vaccine.png')}
+        />
+      </View>
+      <View style={styles.viewContent}>
+        <Title>Vacinação covid-19</Title>
+        <Text style={styles.textContent}>
+          It is a long established fact that a 
+          reader will be distracted by the readable content of a page when 
+          looking at its layout. The point of using Lorem Ipsum is that it has 
+          a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+          content here', making it look like readable English. Many desktop 
+          publishing packages and web page editors now use Lorem Ipsum as their 
+          default model text, and a search for 'lorem ipsum' will uncover many web 
+          sites still in their infancy. Various versions have evolved over the years, 
+          sometimes by accident, sometimes on purpose (injected humour and the like).
+        </Text>
+        <TouchableOpacity 
+          style={styles.buttonHome}
+          onPress={() => navigation.navigate('Scheduling')}
+        >
+          <Text>Agendar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: '#6fbbd3',
+    alignItems: 'center',
+    height: '100%'
+  },
+  viewImage: {
+    paddingTop: 15,
+  },
+  mainImageHome: {
+    width: 200,
+    height: 150
+  },
+  viewContent: {
+    margin: 20,
+    alignItems: 'center',
+  },
+  textContent: {
+    letterSpacing: 1,
+    textAlign: 'center'
+  },
+  buttonHome: {
+    marginTop: 20,
+    borderRadius: 25,
+    backgroundColor: '#DB901A',
+    padding: 10,
+    alignItems: 'center',
+    width: 250
+  }
+});
--- src/screens/scheduling/Scheduling.js
+++ src/screens/scheduling/Scheduling.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, TextInput } from 'react-native';
+import { Title } from 'react-native-paper';
+import { Picker } from '@react-native-picker/picker';
 
 const Scheduling = () => {
+
+  const [cursos] = useState(['Android','NodeJs','Python','PHP','Asp']);
+  const [cursoSelecionando, setCursoSelecionando] = useState([])
+
   return(
-    <View>
-      <Text>Scheduling</Text>
+    <View style={styles.mainViewScheduling}>
+      <View>
+        <Image
+          style={styles.imageScheduling}
+          source={require('../../assets/images/vaccinebanner.png')}
+        />
+      </View>
+      <View>
+        <Picker
+          selectedValue={cursoSelecionando}
+          onValueChange={(itemValue) => 
+            setCursoSelecionando(itemValue)
+          }
+        >
+          {
+            cursos.map(cr => {
+              return <Picker.Item label={cr} value={cr} key={cr} />
+            })
+          }
+        </Picker>
+      </View>
     </View>
   );
 };
 
 export default Scheduling;
+
+const styles = StyleSheet.create({
+  mainViewScheduling: {
+    width: '100%',
+    height: '100%',
+    flex: 1
+  },
+  imageScheduling: {
+    width: '100%',
+    height: 100
+  },  
+  teste: {
+    alignItems: 'center'
+  },
+  aaa: {
+    width: '90%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  }
+});
